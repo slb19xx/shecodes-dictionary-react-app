d4f62047d7288c1888deb30aca40d8368385068f
slb19xx <93225634+slb19xx@users.noreply.github.com>
2022-07-22 01:16:11 -0400

    Styled the application

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="sunset" />
       </main>
       <footer className="App-footer">
         <small>Open Source Code by Shari Baldie</small>
